2030607c7012d78233deb3882f94bdac1fb6aff2
Diogo Azevedo <diogoazevedos@gmail.com>
2016-12-06 13:52:22 -0200

    feat: Add default not found route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const UserController = require('../src/Http/Controllers/User');
 
-const { define, wrap, post, get } = require('spirit-router');
+const { define, wrap, post, get, any, notFound } = require('spirit-router');
 const { json } = require('body-parser');
 const express  = require('spirit-express');
 
-module.exports = define('/api', [
+const api = define('/api', [
   get('/users', UserController.index),
   get('/users/:user_id', ['user_id'], UserController.show),
   wrap(post('/users', ['body'], UserController.store), express(json())),
 ]);
+
+module.exports = define([api, any('*', notFound())]);
